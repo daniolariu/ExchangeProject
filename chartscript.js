3759007f4cade06fd9171c02209c0f69a32ecdaa
daniolariu <olariudaniel177@gmail.com>
2024-04-11 16:44:31 +0300

    update

--- chartscript.js
+++
@@ -1,7 +0,0 @@
-$.getJSON('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd', function(data) {
-    console.log(data);
-    var btcPrice = data.bitcoin.usd;
-    var ethPrice = data.ethereum.usd;
-    document.getElementById("btcprice").innerHTML = btcPrice;
-    document.getElementById("ethprice").innerHTML = ethPrice;
-});
