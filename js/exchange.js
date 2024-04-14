25e38d2d46b9a38e46f82a8985f7a7b798def21e
daniolariu <olariudaniel177@gmail.com>
2024-04-14 15:44:24 +0300

    update

---
+++ js/exchange.js
@@ -0,0 +1,34 @@
+var coin1 = document.getElementById('coin1');
+function autoRefresh() {
+    setTimeout(function () {
+        location.reload();
+    }, 60000); // 30 seconds in milliseconds
+}
+var btcPrice;
+var ethPrice;
+var solPrice;
+var quantBtc = 1.53, quantEth = 5.3, quantSol = 124;
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Csolana&vs_currencies=usd";
+fetch(url)
+  .then(res => res.json())
+  .then(data =>  {
+    console.log(data);
+    btcPrice = data.bitcoin.usd;
+    ethPrice = data.ethereum.usd;
+    solPrice = data.solana.usd;
+    autoRefresh();
+  });
+document.addEventListener('click', function(event) {
+    if(coin1.value == 'bitcoin'){
+       document.getElementById('quantBtc').innerHTML = quantBtc;
+      }
+      else if(coin1.value == 'ethereum'){
+        document.getElementById('quantEth').innerHTML = quantEth;
+      }
+      else if(coin1.value == 'solana'){
+        document.getElementById('quantSol').innerHTML = quantSol;
+      }
+      else{
+        console.log('No coin selected');
+      }
+});
